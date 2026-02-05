36936ecc30dc48d0999bf03084b65d3718b856fe
manishkumar13953 <manish.kumar_cs.aiml24@gla.ac.in>
2026-02-05 08:49:12 +0530

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
-#define ldrPin A0
+#define LDR_Pin A0
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    pinMode(LDR_Pin, INPUT);
+    Serial.println("LDR Test");//Important for Uno R4 WiFi
+
 }
 
 void loop() {
-// write your code here
+    int ldrRaw = analogRead(LDR_Pin);
+    int lightPercent = map(ldrRaw, 0, 1023, 0, 100);
+    Serial.print("LDR Raw: ");
+    Serial.print(ldrRaw);
+    Serial.print(" | Light: ");
+    Serial.print(lightPercent);
+    Serial.println("%");
+    delay(500);
+
 }
